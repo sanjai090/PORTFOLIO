65d7fd54be36d3e1e03cc847ae2f9a306bf231a8
sanjai090 <sanjaisvhec2024@gmail.com>
2026-03-01 15:39:04 +0530

    temp rename

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Education from "./components/Education";
 import Projects from "./components/Projects";
-import Certificates from "./components/Certificates";
+import Certificates from "./components/Temp";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
